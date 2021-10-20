6e005f72aa4debe07c4a88ab989669a2d307b560
Jorge Lara <larajorge11@gmail.com>
2021-10-19 22:12:50 -0500

    feat(First commit)

---
+++ src/main/java/com/davor/di/DavorDependencyInjection/DavorDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.davor.di.DavorDependencyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavorDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavorDependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davor/di/DavorDependencyInjection/DavorDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davor.di.DavorDependencyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavorDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
