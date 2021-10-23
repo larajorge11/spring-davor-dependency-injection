86c4386a3f737ad444299d2cd7b49bd00d5f907d
Jorge Lara <larajorge11@gmail.com>
2021-10-22 19:33:59 -0500

    feat(SOLID: Open/Close Principle: Violating the principle)

---
+++ src/main/java/com/davor/solid/openclose/HealthInsuranceValidator.java
@@ -0,0 +1,9 @@
+package com.davor.solid.openclose;
+
+public class HealthInsuranceValidator {
+    public boolean isValidInsurance() {
+        System.out.println("Validating Health Insurance Claim");
+
+        return true;
+    }
+}
---
+++ src/main/java/com/davor/solid/openclose/VehiculeInsuranceValidator.java
@@ -0,0 +1,9 @@
+package com.davor.solid.openclose;
+
+public class VehiculeInsuranceValidator {
+    public boolean isValidInsurance() {
+        System.out.println("Validating Vehicule Insurance Claim");
+
+        return true;
+    }
+}
