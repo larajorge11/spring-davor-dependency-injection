6412d7da21a67a9874c40ab209ac5cb655446b03
Jorge Lara <larajorge11@gmail.com>
2021-11-04 08:25:08 -0500

    feat(Create xml option config to beans)

--- src/main/java/com/davor/di/DavorDependencyInjection/config/GreetingServiceConfig.java
+++ src/main/java/com/davor/di/DavorDependencyInjection/config/GreetingServiceConfig.java
@@ -3,28 +3,11 @@ package com.davor.di.DavorDependencyInjection.config;
 import com.davor.di.DavorDependencyInjection.repository.InternationalMessages;
 import com.davor.di.DavorDependencyInjection.repository.InternationalMessagesImpl;
 import com.davor.di.DavorDependencyInjection.service.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
-
-    @Bean
-    ConstructorGreetingService constructorGreetingService() {
-        return new ConstructorGreetingService();
-    }
-
-    @Bean
-    PropertyGreetingService propertyGreetingService() {
-        return new PropertyGreetingService();
-    }
-
-    @Bean
-    SetterGreetingService setterGreetingService() {
-        return new SetterGreetingService();
-    }
 
     @Primary
     @Bean
